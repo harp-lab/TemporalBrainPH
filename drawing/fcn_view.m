a31f74a3d61f84826f8b5a71b5fd54b1e37b5b56
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2023-08-31 17:18:26 -0500

    added fcn and extracted barcodes

--- drawing/fcn_view.m
+++
@@ -1,9 +0,0 @@
-dfc_645=load("normalize_dfc_2500_subject_1_time_1.txt");
-imagesc(dfc_645);
-colorbar;
-axis off;
-% dfc_1400=load("normalize_dfc_1400_subject_1_time_1.txt");
-% imagesc(dfc_1400);
-% colorbar
-% dfc_2500=load("normalize_dfc_2500_subject_1_time_1.txt");
-% imagesc(dfc_2500);
